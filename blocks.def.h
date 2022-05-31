e4fe714fd2ac3277359d2f2677e397005e94dd46
Jerry Wang <freebufer.wang@gmail.com>
2022-05-31 23:20:07 +0800

    Update 'blocks.def.h'.

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,15 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-        /* { "", "dwmMusic", 0, 1}, */
-        /* { "", "dwmNet", 1, 2}, */
-        { "", "dwmMailbox", 1800, 3 },
-        { "", "dwmMemory", 60, 11},
-        { "", "dwmDisk", 60, 4},
-        { "", "dwmWeather", 3600, 6},
+        { "", "dwmMusic", 0, 1 },
+        /* { "", "dwmNet", 1, 2 }, */
+        { "", "dwmMailbox", 0, 3 },
+        { "", "dwmMemory", 60, 11 },
+        { "", "dwmDisk", 60, 4 },
+        { "", "dwmWeather", 3600, 6 },
         { "", "dwmClock", 60, 5 },
-        { "", "dwmVol", 0, 7 },
         { "", "dwmBattery", 60, 8 },
+        { "", "dwmVol", 0, 7 },
         { "", "dwmUpdate", 0, 9 },
         { "", "dwmMoon", 3600, 10 },
 };
